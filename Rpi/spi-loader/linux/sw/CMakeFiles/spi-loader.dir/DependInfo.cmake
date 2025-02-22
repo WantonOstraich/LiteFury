707e1adaf7aef216bbbeadb1f8a28229cf03e48f
WantonOstraich <jerome.massol@gmail.com>
2025-02-22 17:04:39 -0500

    update repo

---
+++ Rpi/spi-loader/linux/sw/CMakeFiles/spi-loader.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/jerome/Git/NiteFury-and-LiteFury-2.0/spi-loader/linux/sw/lib/common/xil_assert.c" "CMakeFiles/spi-loader.dir/lib/common/xil_assert.c.o" "gcc" "CMakeFiles/spi-loader.dir/lib/common/xil_assert.c.o.d"
+  "/home/jerome/Git/NiteFury-and-LiteFury-2.0/spi-loader/linux/sw/lib/common/xil_io.c" "CMakeFiles/spi-loader.dir/lib/common/xil_io.c.o" "gcc" "CMakeFiles/spi-loader.dir/lib/common/xil_io.c.o.d"
+  "/home/jerome/Git/NiteFury-and-LiteFury-2.0/spi-loader/linux/sw/lib/src/xspi.c" "CMakeFiles/spi-loader.dir/lib/src/xspi.c.o" "gcc" "CMakeFiles/spi-loader.dir/lib/src/xspi.c.o.d"
+  "/home/jerome/Git/NiteFury-and-LiteFury-2.0/spi-loader/linux/sw/lib/src/xspi_options.c" "CMakeFiles/spi-loader.dir/lib/src/xspi_options.c.o" "gcc" "CMakeFiles/spi-loader.dir/lib/src/xspi_options.c.o.d"
+  "/home/jerome/Git/NiteFury-and-LiteFury-2.0/spi-loader/linux/sw/lib/src/xspi_stats.c" "CMakeFiles/spi-loader.dir/lib/src/xspi_stats.c.o" "gcc" "CMakeFiles/spi-loader.dir/lib/src/xspi_stats.c.o.d"
+  "/home/jerome/Git/NiteFury-and-LiteFury-2.0/spi-loader/linux/sw/app-linux/Main.cpp" "CMakeFiles/spi-loader.dir/app-linux/Main.cpp.o" "gcc" "CMakeFiles/spi-loader.dir/app-linux/Main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Rpi/spi-loader/linux/sw/CMakeFiles/spi-loader.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spi-loader.dir/app-linux/Main.cpp.o"
+  "CMakeFiles/spi-loader.dir/app-linux/Main.cpp.o.d"
+  "CMakeFiles/spi-loader.dir/lib/common/xil_assert.c.o"
+  "CMakeFiles/spi-loader.dir/lib/common/xil_assert.c.o.d"
+  "CMakeFiles/spi-loader.dir/lib/common/xil_io.c.o"
+  "CMakeFiles/spi-loader.dir/lib/common/xil_io.c.o.d"
+  "CMakeFiles/spi-loader.dir/lib/src/xspi.c.o"
+  "CMakeFiles/spi-loader.dir/lib/src/xspi.c.o.d"
+  "CMakeFiles/spi-loader.dir/lib/src/xspi_options.c.o"
+  "CMakeFiles/spi-loader.dir/lib/src/xspi_options.c.o.d"
+  "CMakeFiles/spi-loader.dir/lib/src/xspi_stats.c.o"
+  "CMakeFiles/spi-loader.dir/lib/src/xspi_stats.c.o.d"
+  "spi-loader"
+  "spi-loader.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/spi-loader.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
